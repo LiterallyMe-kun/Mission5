f0b9483d66001fd4c19db0780ccff40ccae24dd1
LiterallyMe-kun <sknoah2002@gmail.com>
2025-02-12 13:56:28 -0700

    yes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $("#calculateBtn").click(function () {
+        let hours = $("#hours").val();
+        let rate = parseFloat($("#rate").val());
+
+        // Validate input
+        if (!hours || hours <= 0) {
+            $("#hoursError").show();
+            $("#total").val("");
+            return;
+        } else {
+            $("#hoursError").hide();
+        }
+
+        // Calculate total
+        let total = hours * rate;
+        $("#total").val(total.toFixed(2));
+    });
+
+    // Hide error message when user starts typing
+    $("#hours").on("input", function () {
+        $("#hoursError").hide();
+    });
+});
